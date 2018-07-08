d381fb4d8fd03b1735ab4d405a9dca077244ff64
antuanoro <antonio_manteca@hotmail.com>
2018-07-08 11:03:38 +0000

    Fix test integracion

--- tests/integration/API_Test.js
+++ tests/integration/API_Test.js
@@ -14,16 +14,14 @@ describe('Pruebas contra API REST del entorno integrado', function () {
 	
 	// Primero debemos limpiar la BBDD
 	before(function (done) {
-		it('limpiamos todos los empleados', (done) => {
-			chai.request(server)
-				.delete('/employees')
-				.end((err, res) => {
-					res.should.have.status(200);
-					res.body.should.be.a('object');
-					res.body.should.have.property('message').eql('Empleados borrados correctamente');
-					done();
-				});
-		});
+		chai.request(server)
+			.delete('/employees')
+			.end((err, res) => {
+				res.should.have.status(200);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message').eql('Empleados borrados correctamente');
+				done();
+			});
 	});
 	
 	// Probamos GET
