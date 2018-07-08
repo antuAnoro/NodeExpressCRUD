8bdf772f42cbb66396552d1da20f44ca96c63d6b
antuanoro <antonio_manteca@hotmail.com>
2018-07-08 11:02:08 +0200

    Conversion a API REST

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ mongoose.connect(`mongodb://${ip_bbdd}/employee`)
   .catch((err) => console.error(err));
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var employees = require('./routes/employees');
 
 var app = express();
@@ -24,8 +23,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/employees', employees);
 
 // catch 404 and forward to error handler
--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -4,81 +4,69 @@ var Employee = require("../models/Employee");
 var employeeController = {};
 
 // Show list of employees
-employeeController.list = function(req, res) {
+employeeController.getAll = function(req, res) {
   Employee.find({}).exec(function (err, employees) {
     if (err) {
       console.log("Error:", err);
+	  res.send(err);
     }
     else {
-      res.render("../views/employees/index", {employees: employees});
+      res.json(employees);
     }
   });
 };
 
 // Show employee by id
-employeeController.show = function(req, res) {
+employeeController.get = function(req, res) {
   Employee.findOne({_id: req.params.id}).exec(function (err, employee) {
     if (err) {
       console.log("Error:", err);
+	  res.send(err);
     }
     else {
-      res.render("../views/employees/show", {employee: employee});
+      res.json(employee);
     }
   });
 };
 
 // Create new employee
-employeeController.create = function(req, res) {
-  res.render("../views/employees/create");
-};
-
-// Save new employee
 employeeController.save = function(req, res) {
   var employee = new Employee(req.body);
 
-  employee.save(function(err) {
+  employee.save(function(err, employee) {
     if(err) {
-      console.log(err);
-      res.render("../views/employees/create");
-    } else {
-      console.log("Successfully created an employee.");
-      res.redirect("/employees/show/"+employee._id);
-    }
-  });
-};
-
-// Edit an employee
-employeeController.edit = function(req, res) {
-  Employee.findOne({_id: req.params.id}).exec(function (err, employee) {
-    if (err) {
       console.log("Error:", err);
-    }
-    else {
-      res.render("../views/employees/edit", {employee: employee});
+      res.send(err);
+    } else {
+      console.log("Empleado creado correctamente.");
+      res.json({message: "Empleado creado correctamente", employee });
     }
   });
 };
 
 // Update an employee
-employeeController.update = function(req, res) {
+employeeController.edit = function(req, res) {
   Employee.findByIdAndUpdate(req.params.id, { $set: { name: req.body.name, address: req.body.address, position: req.body.position, salary: req.body.salary }}, { new: true }, function (err, employee) {
     if (err) {
-      console.log(err);
-      res.render("../views/employees/edit", {employee: req.body});
-    }
-    res.redirect("/employees/show/"+employee._id);
+      console.log("Error:", err);
+      res.send(err);
+    } else {
+      console.log("Empleado actualizado correctamente.");
+      res.json({message: "Empleado actualizado correctamente", employee });
+	}
   });
 };
 
 // Delete an employee
 employeeController.delete = function(req, res) {
-  Employee.remove({_id: req.params.id}, function(err) {
+  Employee.remove({_id: req.params.id}, function(err, result) {
     if(err) {
-      console.log(err);
+      console.log("Error:", err);
+      res.send(err);
     }
     else {
-      console.log("Employee deleted!");
-      res.redirect("/employees");
+      console.log("Empleado borrado correctamente.");
+      res.json({message: "Empleado borrado correctamente", result });
     }
   });
 };
