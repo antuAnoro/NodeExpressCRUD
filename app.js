c9c97ccc632facf46f46cccda486769f8bb4478c
antuanoro <antonio_manteca@hotmail.com>
2018-07-22 17:20:02 +0200

    Me llevo los docs a public

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/docs/index.html'));
+    res.sendFile(path.join(__dirname + '/public/docs/index.html'));
 });
 
 app.use('/employees', employees);
